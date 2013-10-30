8d01e80dad63a04d26cdccebb8653878ec1a4089
gman <gman@codefreax.org>
2013-10-30 18:51:10 +0100

    added Column.cpp

---
+++ src/Column.cpp
@@ -0,0 +1,34 @@
+/*
+	Copyright (C) 2013 Róbert "gman" Vašek <gman@codefreax.org>
+
+	This file is part of libhawk.
+
+	libhawk is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+
+	libhawk is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with libhawk.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Column.h"
+
+using namespace hawk;
+
+Column::Column(const boost::filesystem::path& path,
+	Type_factory::Type_product tp)
+	: m_path{path}
+{
+	m_handler = tp();
+}
+
+Column::~Column()
+{
+	delete m_handler;
+}
