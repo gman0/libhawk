67e5f0ed441ce6677b18848044355594b7521efa
gman <gman@codefreax.org>
2013-11-23 14:41:29 +0100

    added an extern get_handler_hash<> header

---
+++ src/include/handlers/dir_hash_extern.h
@@ -0,0 +1,29 @@
+/*
+	Copyright (C) 2013 Róbert "gman" Vašek <gman@codefreax.org>
+
+	This file is part of libhawk.
+
+	libhawk is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+
+	libhawk is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with libhawk.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef HAWK_LIST_DIR_HASH_EXTERN_H
+#define HAWK_LIST_DIR_HASH_EXTERN_H
+
+#include "handlers/hash.h"
+
+namespace hawk {
+	extern template size_t get_handler_hash<List_dir>();
+}
+
+#endif // HAWK_LIST_DIR_HASH_EXTERN_H
