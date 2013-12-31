04a1e6def3d74317ac95d1a56e0ae3e0f007c9b5
gman <gman@codefreax.org>
2013-12-31 16:07:20 +0100

    added No_hash struct

---
+++ src/NoHash.cpp
@@ -0,0 +1,26 @@
+/*
+	Copyright (C) 2013 Róbert "gman" Vašek <gman@codefreax.org>
+
+	This file is part of libhawk.
+
+	libhawk is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+
+	libhawk is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with libhawk.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <cstddef>
+#include "NoHash.h"
+
+std::size_t hawk::No_hash::operator()(const size_t& key) const
+{
+	return key;
+}
